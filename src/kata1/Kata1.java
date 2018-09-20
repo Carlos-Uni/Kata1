95afa670c132630b1dc875bbff9ec6b6d87cbce1
Usuario <Usuario@10.22.148.98>
2018-09-20 09:25:19 +0100

    Primera version de Kata1 Corregida(1.1)

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -6,6 +6,7 @@ public class Kata1 {
 
     public static void main(String[] args) {
         Person person = new Person("Carlos", new Date(98,11,14));
+        System.out.print(person.getName() + " tiene "+ person.getAge());
     }
     
 }
