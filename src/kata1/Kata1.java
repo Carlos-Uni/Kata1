8e18fb86c650a5ca27f1acdce36143f54c3f6ceb
Usuario <Usuario@10.22.148.98>
2018-09-20 09:13:09 +0100

    Primera version de Kata1

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,11 @@
+package kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person person = new Person("Carlos", new Date(98,11,14));
+    }
+    
+}
